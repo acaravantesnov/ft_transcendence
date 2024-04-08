8455e57ac497a3bdaa227a643f239fd26d370ef0
dag00dag33 <dag00dag33@gmail.com>
2024-04-08 20:45:21 +0200

    updated to newer python

--- TranscendenceApp/models.py
+++ TranscendenceApp/models.py
@@ -1,6 +1,42 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length = 250)
     email = models.CharField(max_length = 250)
+
+
+class Game(models.Model):
+    # Choices for game status
+    ONGOING = 'ongoing'
+    COMPLETED = 'completed'
+    GAME_STATUS_CHOICES = [
+        (ONGOING, 'Ongoing'),
+        (COMPLETED, 'Completed'),
+    ]
+
+    # Player 1 and Player 2 fields as foreign keys to the User model
+    player1 = models.ForeignKey(User, related_name='player1', on_delete=models.CASCADE)
+    player2 = models.ForeignKey(User, related_name='player2', on_delete=models.CASCADE)
+
+    # Fields to store player scores
+    player1_score = models.IntegerField()
+    player2_score = models.IntegerField()
+
+    # Field to store game status
+    game_status = models.CharField(max_length=50, choices=GAME_STATUS_CHOICES, default=ONGOING)
+
+    # Field to store the time the game started
+    time_started = models.DateTimeField(default=timezone.now)
+
+    # Field to store the time the game ended
+    time_ended = models.DateTimeField(null=True, blank=True)
+
+    def end_game(self):
+        """
+        Method to end the game and update game status and end time.
+        """
+        self.game_status = self.COMPLETED
+        self.time_ended = timezone.now()
+        self.save()
--- django.sh
+++ django.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Wait for the postgres container to be ready before running the Django migrations
+echo "Waiting for postgres..."
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+sleep 5
+
+# Verificar si el superusuario ya existe
+if python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='admin').exists())" | grep -q "True"; then
+    echo "Superuser already exists"
+else
+    # Crear el superusuario
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
+    echo "Superuser created successfully (username: admin, password: password)"
+fi
+
 echo "Creating Migrations..."
 python manage.py makemigrations TranscendenceApp
 echo ====================================
