3ece89a2d99c2107e6073848eec303f8f94ab121
DAG00DAG33 <32079160+DAG00DAG33@users.noreply.github.com>
2024-04-08 20:40:52 +0200

    Mortega (#1)
    * Docker Network
    
    * Docker Network added
    
    ---------
    
    Co-authored-by: Diego antolin Garcia soto <digarcia@c3r5s1.42madrid.com>

--- TranscendenceApp/models.py
+++ TranscendenceApp/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length = 250)
     email = models.CharField(max_length = 250)
+
+class Test(models.Model):
+    name = models.CharField(max_length = 250)
+    lm = models.CharField(max_length = 250)
